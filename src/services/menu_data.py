141571e8580bb60b26532cf193ca461bc56e257e
karl-no <karl.publicidade@gmail.com>
2023-05-16 15:39:50 -0300

    feat: MenuData class ready

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,24 @@
+import pandas as pd
+from models.dish import Dish, Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.df = pd.read_csv(source_path)
+
+        dishes = {}
+
+        for data in self.df.itertuples(index=False):
+            dish, price, ingredient, recipe_amount = data
+            if dish not in dishes:
+                dish_info = Dish(dish, price)
+                dishes[dish] = dish_info
+                self.dishes.add(dish_info)
+
+            ingredient_info = Ingredient(ingredient)
+            dishes[dish].add_ingredient_dependency(
+                ingredient_info,
+                recipe_amount
+            )
