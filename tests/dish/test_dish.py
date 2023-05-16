f86e7d49c1b1a7f1a1d98f5ac28c999241a4b8ff
karl-no <karl.publicidade@gmail.com>
2023-05-16 15:09:50 -0300

    test: class Dish tested + changes in mock inside test_ingredient

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,34 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    ingredient1 = Ingredient("ovo")
+    dish1 = Dish("bolo", 20.00)
+    dish2 = Dish("rocambole", 10.00)
+
+    dish1.add_ingredient_dependency(ingredient1, 1)
+
+    assert dish1.name == "bolo"
+
+    assert hash(dish1) == hash(dish1)
+    assert hash(dish1) != hash(dish2)
+
+    assert dish1.__eq__(dish1) is True
+    assert dish1.__eq__(dish2) is False
+
+    assert repr(dish1) == "Dish('bolo', R$20.00)"
+
+    with pytest.raises(TypeError):
+        Dish("bolo", "biscoito")
+
+    with pytest.raises(ValueError):
+        Dish("bolo", 0)
+
+    assert dish1.get_ingredients() == {
+        Ingredient("ovo")
+    }
+
+    assert dish1.get_restrictions() == {Restriction.ANIMAL_DERIVED}
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,19 +1,19 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (Ingredient, Restriction)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    ingredient1 = Ingredient("strombelete")
-    ingredient2 = Ingredient("cream cheese")
+    ingredient1 = Ingredient("ovo")
+    ingredient2 = Ingredient("farinha")
 
-    assert hash(ingredient1) == hash("strombelete")
+    assert hash(ingredient1) == hash("ovo")
     assert hash(ingredient1) != hash(ingredient2)
 
     assert ingredient1 == ingredient1
     assert ingredient1 != ingredient2
 
-    assert repr(ingredient1) == "Ingredient('strombelete')"
+    assert repr(ingredient1) == "Ingredient('ovo')"
 
-    assert ingredient1.name == "strombelete"
+    assert ingredient1.name == "ovo"
 
-    assert ingredient1.restrictions == set()
+    assert ingredient1.restrictions == {Restriction.ANIMAL_DERIVED}
