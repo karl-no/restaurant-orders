752a3743ec4e5e495ae3fc1bc0254087fabfb71e
karl-no <karl.publicidade@gmail.com>
2023-05-18 10:31:27 -0300

    feat: check_recipe_availability and consume_recipe functions, in inventory_control.py, implemented

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -27,8 +27,19 @@ class InventoryMapping:
 
     # Req 5.1
     def check_recipe_availability(self, recipe: Recipe):
-        pass
+        for ingredient in recipe:
+            if ingredient not in self.inventory or int(
+                recipe[ingredient]
+            ) > self.inventory[ingredient]:
+                return False
+        return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        if not recipe:
+            return None
+        if not self.check_recipe_availability(recipe):
+            raise ValueError
+        else:
+            for ingredient in recipe:
+                self.inventory[ingredient] -= int(recipe[ingredient])
